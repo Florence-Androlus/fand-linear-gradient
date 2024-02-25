cdc1bad1fe62413e2e42ccecc906bcfff0275806
androlus <androlus@9online.fr>
2024-02-25 19:04:03 +0100

    ajout licence GNU v3.0

--- fand-linear-gradient.php
+++ fand-linear-gradient.php
@@ -6,8 +6,7 @@
  * Requires PHP:      7.0
  * Version:           0.1.0
  * Author:            The WordPress Contributors
- * License:           GPL-2.0-or-later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * License:           GNU v3.0
  * Text Domain:       fand-linear-gradient
  *
  * @package           create-block
@@ -21,8 +20,6 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Registers the block using the metadata loaded from the `block.json` file.
  * Behind the scenes, it registers also all assets so they can be enqueued
  * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
 function fand_gradient_fand_gradient_block_init() {
 	register_block_type( __DIR__ . '/build' );
